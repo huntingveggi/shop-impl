e0191471794508923a59bce27273a378a315ef77
Stefan Stahl <red_steel@web.de>
2014-02-13 19:15:05 +0100

    Add Annotations to InvoiceImpl

--- src/main/java/de/is/project/shop/impl/domain/InvoiceImpl.java
+++ src/main/java/de/is/project/shop/impl/domain/InvoiceImpl.java
@@ -3,20 +3,27 @@ package de.is.project.shop.impl.domain;
 import java.util.Collection;
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
 import de.is.project.shop.api.domain.Invoice;
 import de.is.project.shop.api.domain.OrderItem;
 
+@Entity(name="Invoice")
 public class InvoiceImpl extends AbstractEntity implements Invoice {
 
 	Date invoiceDate;
 	Collection<OrderItem> orderItems;
 
 	@Override
+	@OneToMany(targetEntity=OrderItemImpl.class)
 	public Collection<OrderItem> getOrderItems() {
 		return this.orderItems;
 	}
 
 	@Override
+	@Column(name = "InvoiceDate", nullable = false)
 	public Date getInvoiceDate() {
 		return this.invoiceDate;
 	}
