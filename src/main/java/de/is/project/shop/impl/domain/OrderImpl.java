17256170e81e65537211df1ef554ed201c5728bc
Stefan Stahl <red_steel@web.de>
2014-02-14 16:21:47 +0100

    Adjust OrderImpl: implemnt Visitable

--- src/main/java/de/is/project/shop/impl/domain/OrderImpl.java
+++ src/main/java/de/is/project/shop/impl/domain/OrderImpl.java
@@ -15,9 +15,11 @@ import de.is.project.shop.api.domain.Address;
 import de.is.project.shop.api.domain.Customer;
 import de.is.project.shop.api.domain.Order;
 import de.is.project.shop.api.domain.OrderItem;
+import de.is.project.shop.api.domain.Visitable;
+import de.is.project.shop.api.domain.Visitor;
 
 @Entity(name = "orders")
-public class OrderImpl extends AbstractEntity implements Order {
+public class OrderImpl extends AbstractEntity implements Order, Visitable {
 
 	Collection<OrderItem> items = new LinkedList<>();;
 	Date orderDate;
@@ -114,6 +116,11 @@ public class OrderImpl extends AbstractEntity implements Order {
 	@Override
 	public void setTotal(double total) {
 		this.total = total;
+	}
+
+	@Override
+	public void accept(Visitor visitor) {
+		visitor.visit(this);		
 	}
 
 }
