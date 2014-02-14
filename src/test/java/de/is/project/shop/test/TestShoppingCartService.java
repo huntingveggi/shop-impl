1501468cfafe2551ef48e76acf0c7a924c636265
Stefan Stahl <red_steel@web.de>
2014-02-14 16:35:47 +0100

    Adjust TestShoppingCartService
    - now uses spring test framework

--- src/test/java/de/is/project/shop/test/TestShoppingCartService.java
+++ src/test/java/de/is/project/shop/test/TestShoppingCartService.java
@@ -1,25 +1,30 @@
 package de.is.project.shop.test;
 
-import static org.junit.Assert.*;
+import javax.inject.Inject;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.Assert;
 
 import de.is.project.shop.api.domain.Product;
 import de.is.project.shop.api.domain.ShoppingCart;
 import de.is.project.shop.api.services.*;
 import de.is.project.shop.impl.domain.ProductImpl;
 import de.is.project.shop.impl.domain.ShoppingCartImpl;
-import de.is.project.shop.impl.services.ShoppingCartServiceImpl;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:spring.xml" })
 public class TestShoppingCartService {
 	
+	@Inject
 	private ShoppingCartService scs;
 	
 	@Before
 	public void setUp() {
 		
-		scs = new ShoppingCartServiceImpl();
 		ShoppingCart cart = new ShoppingCartImpl();
 		scs.setShoppingCart(cart);
 	}
@@ -34,8 +39,8 @@ public class TestShoppingCartService {
 		product1.setPrice(10.0);
 		scs.addProduct(product1);
 		scs.addProduct(product1);
-		assertEquals(1, scs.getShoppingCart().getShoppingCartPositions().size());
-		assertEquals(20, scs.getShoppingCart().getTotal(), 0);
+		Assert.isTrue(scs.getShoppingCart().getShoppingCartPositions().size() == 1);
+		Assert.isTrue(scs.getShoppingCart().getTotal() == 20);
 		
 		Product product3 = new ProductImpl();
 		product3.setName("Kuehlschrank2");
@@ -43,25 +48,25 @@ public class TestShoppingCartService {
 		product3.setId(2);
 		product3.setPrice(13.0);
 		scs.addProduct(product3);
-		assertEquals(2, scs.getShoppingCart().getShoppingCartPositions().size());
-		assertEquals(33, scs.getShoppingCart().getTotal(), 0);
+		Assert.isTrue(scs.getShoppingCart().getShoppingCartPositions().size() == 2);
+		Assert.isTrue(scs.getShoppingCart().getTotal() == 33);
 		
 		scs.removeProduct(product1);
-		assertEquals(2, scs.getShoppingCart().getShoppingCartPositions().size());
-		assertEquals(23, scs.getShoppingCart().getTotal(), 0);
+		Assert.isTrue(scs.getShoppingCart().getShoppingCartPositions().size() == 2);
+		Assert.isTrue(scs.getShoppingCart().getTotal() == 23);
 		
 		scs.removeProduct(product1);
-		assertEquals(1, scs.getShoppingCart().getShoppingCartPositions().size());
-		assertEquals(13, scs.getShoppingCart().getTotal(), 0);
+		Assert.isTrue(scs.getShoppingCart().getShoppingCartPositions().size() == 1);
+		Assert.isTrue(scs.getShoppingCart().getTotal() == 13);
 		
 		scs.removeProduct(product3);
-		assertEquals(0, scs.getShoppingCart().getShoppingCartPositions().size());
-		assertEquals(0, scs.getShoppingCart().getTotal(), 0);
+		Assert.isTrue(scs.getShoppingCart().getShoppingCartPositions().size() == 0);
+		Assert.isTrue(scs.getShoppingCart().getTotal() == 0);
 		
 		scs.addProduct(product1);
 		scs.resetShoppingCart();
-		assertEquals(0, scs.getShoppingCart().getShoppingCartPositions().size());
-		assertEquals(0, scs.getShoppingCart().getTotal(), 0);
+		Assert.isTrue(scs.getShoppingCart().getShoppingCartPositions().size() == 0);
+		Assert.isTrue(scs.getShoppingCart().getTotal() == 0);
 	}
 	
 
