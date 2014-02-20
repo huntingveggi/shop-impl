d36f767df047557030e28247895ee8d3fdfc7f92
Stefan Stahl <red_steel@web.de>
2014-02-20 20:02:27 +0100

    Adjust MessageImpl according to interface change

--- src/main/java/de/is/project/shop/impl/domain/MessageImpl.java
+++ src/main/java/de/is/project/shop/impl/domain/MessageImpl.java
@@ -13,19 +13,7 @@ public class MessageImpl extends AbstractEntity implements Message {
 	boolean isFromAdmin;
 
 	@Override
-	public boolean getIsFromAdmin() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean getIsRead() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	@ManyToOne(targetEntity=RequestImpl.class)
+	@ManyToOne(targetEntity = RequestImpl.class)
 	public Request getRequest() {
 		return request;
 	}
@@ -35,18 +23,22 @@ public class MessageImpl extends AbstractEntity implements Message {
 		return text;
 	}
 
+	@Override
 	public boolean isFromAdmin() {
 		return isFromAdmin;
 	}
 
+	@Override
 	public boolean isRead() {
 		return isRead;
 	}
 
+	@Override
 	public void setFromAdmin(boolean isFromAdmin) {
 		this.isFromAdmin = isFromAdmin;
 	}
 
+	@Override
 	public void setRead(boolean isRead) {
 		this.isRead = isRead;
 	}
