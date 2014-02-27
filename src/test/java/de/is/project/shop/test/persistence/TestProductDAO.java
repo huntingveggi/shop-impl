9fa25b36e3480048fce9dea12dfdfdbf2618a3f4
Dennis Ahaus <dennis.ahaus@gmail.com>
2014-02-27 20:11:28 +0100

    Add @TransactionConfiguration to invoke a default rolleback

--- src/test/java/de/is/project/shop/test/persistence/TestProductDAO.java
+++ src/test/java/de/is/project/shop/test/persistence/TestProductDAO.java
@@ -1,7 +1,5 @@
 package de.is.project.shop.test.persistence;
 
-import java.util.Collection;
-
 import javax.inject.Inject;
 
 import org.junit.After;
@@ -9,14 +7,9 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 
 import de.is.project.shop.api.domain.Category;
@@ -27,7 +20,7 @@ import de.is.project.shop.impl.domain.ProductImpl;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath*:spring.xml" })
-@TransactionConfiguration(defaultRollback = false)
+@TransactionConfiguration(defaultRollback = true)
 public class TestProductDAO {
 
 	@Inject
@@ -49,15 +42,16 @@ public class TestProductDAO {
 
 	public static void tearDownAfterClass() {
 
-		String[] config = TestProductDAO.class.getAnnotation(
-				ContextConfiguration.class).locations();
-		ApplicationContext context = new ClassPathXmlApplicationContext(config);
-		((ConfigurableApplicationContext) context).registerShutdownHook();
-		ProductDAO productDao = context.getBean(ProductDAO.class);
-		Collection<Product> products = productDao.findAll();
-		for (Product product : products) {
-			productDao.delete(product);
-		}
+		// String[] config = TestProductDAO.class.getAnnotation(
+		// ContextConfiguration.class).locations();
+		// ApplicationContext context = new
+		// ClassPathXmlApplicationContext(config);
+		// ((ConfigurableApplicationContext) context).registerShutdownHook();
+		// ProductDAO productDao = context.getBean(ProductDAO.class);
+		// Collection<Product> products = productDao.findAll();
+		// for (Product product : products) {
+		// productDao.delete(product);
+		// }
 	}
 
 	@Test
