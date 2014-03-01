15729d93fa9e7019ffd3362d0582f8117f8ff774
a_JAguar <weber.jonas@gmail.com>
2014-03-01 16:09:56 +0100

    RequestService Impletation

---
+++ src/main/java/de/is/project/shop/impl/services/RequestServiceImpl.java
@@ -0,0 +1,77 @@
+package de.is.project.shop.impl.services;
+
+import javax.inject.Named;
+
+import org.springframework.context.annotation.Scope;
+
+import de.is.project.shop.api.domain.Product;
+import de.is.project.shop.api.domain.Request;
+import de.is.project.shop.api.domain.RequestItem;
+import de.is.project.shop.api.domain.Visitor;
+import de.is.project.shop.api.services.RequestService;
+import de.is.project.shop.impl.domain.RequestItemImpl;
+import de.is.project.shop.impl.domain.VisitorImpl;
+
+@Named
+@Scope("prototype")
+public class RequestServiceImpl implements RequestService {
+
+	private Request request;
+
+	@Override
+	public void addProduct(Product product) {
+		for (RequestItem item : request.getItems()) {
+			if (item.getProduct().getId() == product.getId()) {
+				item.setQuantity((item.getQuantity()) + 1);
+				return;
+			}
+		}
+		RequestItem requestItem = new RequestItemImpl();
+		requestItem.setProduct(product);
+		request.getItems().add(requestItem);
+	}
+
+	@Override
+	public void removeProduct(Product product) {
+		RequestItem removeItem = new RequestItemImpl();
+		for (RequestItem item : request.getItems()) {
+			if (item.getProduct().getId() == product.getId()) {
+				if (item.getQuantity() > 1) {
+					item.setQuantity(item.getQuantity() + 1);
+					return;
+				} else {
+					removeItem = item;
+				}
+				break;
+			}
+		}
+		if (request.getItems().contains(removeItem)) {
+			request.getItems().remove(removeItem);
+		}
+	}
+
+	@Override
+	public void resetRequest() {
+		this.getRequest().getItems().clear();
+		refreshRequest();
+	}
+
+	@Override
+	public void refreshRequest() {
+		Visitor visitor = new VisitorImpl();
+		this.request.accept(visitor);
+	}
+
+	@Override
+	public void setRequest(Request request) {
+		if (this.request == null) {
+			this.request = request;
+		}
+	}
+
+	@Override
+	public Request getRequest() {
+		return this.request;
+	}
+
+}
