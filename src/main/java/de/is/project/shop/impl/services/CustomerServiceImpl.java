faf198c34ab9e2c8b6f9d3112781de54200b9391
Stefan Stahl <red_steel@web.de>
2014-03-01 10:11:50 +0100

    Add TestCustomerService - testActivateCustomer works

--- src/main/java/de/is/project/shop/impl/services/CustomerServiceImpl.java
+++ src/main/java/de/is/project/shop/impl/services/CustomerServiceImpl.java
@@ -4,6 +4,8 @@ import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.springframework.context.annotation.Scope;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import de.is.project.shop.api.domain.Customer;
 import de.is.project.shop.api.persistence.CustomerDAO;
@@ -17,6 +19,7 @@ public class CustomerServiceImpl implements CustomerService {
 	CustomerDAO customerDAO;
 	
 	@Override
+	@Transactional(propagation=Propagation.REQUIRES_NEW)
 	public boolean activateCustomerWithKey(String activationKey) {
 		
 		Customer customer = customerDAO.findByActivationKey(activationKey);
